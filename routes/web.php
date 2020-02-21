a87bb88556fc3d91f96415169a41b6f2edd70ac3
Ali <ali@nadyne.com>
2020-02-21 18:26:17 +0700

    lanjut guard

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm');
+Route::get('/login/member', 'Auth\LoginController@showMemberLoginForm');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm');
+Route::get('/register/member', 'Auth\RegisterController@showMemberRegisterForm');
 
-Auth::routes();
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+Route::post('/login/member', 'Auth\LoginController@MemberLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin');
+Route::post('/register/member', 'Auth\RegisterController@createMember');
 
 Route::get('/home', 'HomeController@index')->name('home');
